1178cf04588dd21845bb5c7682fb0c67e3c2310d
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2024-01-02 00:07:44 +0200

    Applying Dry Principle

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
 'use strict';
 
-// let mySecretNumber = Math.trunc(Math.random() * 20);
-
-// //Elements
-// let checkBtn = document.querySelector('.check');
-// let againBtn = document.querySelector('.again');
-// let messageElement = document.querySelector('.message');
-// let numberElement = document.querySelector('.number');
-// let myBody = document.querySelector('body');
-
-// const checkFunHandler = function () {
-//   const guess = Number(document.querySelector('.guess').value);
-//   if (!guess || guess == 0) {
-//     messageElement.textContent = 'No umber 🙄';
-//   } else if (guess < mySecretNumber) {
-//     messageElement.textContent = 'To low 🙃';
-//   } else if (guess > mySecretNumber) {
-//     messageElement.textContent = 'To high 🙃';
-//   } else if (guess == mySecretNumber) {
-//     messageElement.textContent = 'Congrats ㊗️';
-//     myBody.style.backgroundColor = '#60b347';
-//     numberElement.textContent = mySecretNumber;
-//     numberElement.style.width = '30rem';
-//   } else {
-//     messageElement.textContent = 'Something goes wrong 🤢';
-//   }
-// };
-
-// const againFunHandler = function () {
-//   console.log('hey again');
-//   mySecretNumber = 0;
-//   mySecretNumber = Math.trunc(Math.random() * 20);
-//   myBody.style.backgroundColor = '#222';
-//   numberElement.style.width = '15rem';
-// };
-
-// checkBtn.addEventListener('click', checkFunHandler);
-// againBtn.addEventListener('click', againFunHandler);
-
 const checckBtn = document.querySelector('.check');
 let message = document.querySelector('.message').textContent;
 let secretNumber = Number(Math.trunc(Math.random() * 20) + 1);
-console.log(secretNumber);
+
+let displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 let score = 20;
 let highscore = 0;
 
@@ -50,11 +15,11 @@ const checkFunHandler = function () {
 
   //When there is no input
   if (!guessInputVal) {
-    document.querySelector('.message').textContent = 'Please enter a number⛔';
+    displayMessage('Please enter a number⛔');
 
     //When player wins
   } else if (guessInputVal === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number...! 🧑‍✈️';
+    displayMessage('Correct Number...! 🧑‍✈️');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
@@ -64,22 +29,13 @@ const checkFunHandler = function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    //When the input is too high
-  } else if (guessInputVal > secretNumber) {
+    //When the input is Wrong
+  } else if (guessInputVal !== secretNumber) {
     if (score > 1) {
       score--;
-      document.querySelector('.message').textContent = 'Too high 😮';
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game 😷';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //When the input is too low
-  } else if (guessInputVal < secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.message').textContent = 'Too Low 😮';
+      displayMessage(
+        guessInputVal > secretNumber ? 'Too high 😮' : 'Too Low 😮'
+      );
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'You lost the game 😷';
